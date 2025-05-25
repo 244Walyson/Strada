b080143bdeb3d211266f528fb223996f5d365f9b
Tiago1227 <tiago1227tc@gmail.com>
2025-05-25 12:20:00 -0300

    feat(mobile): implement navigation structure and routing

---
+++ src/strada/mobile/src/app/(tabs)/_layout.tsx
@@ -0,0 +1,47 @@
+<<<<<<< HEAD
+// import React from "react";
+// import { Tabs } from "expo-router";
+// import TabBar from "@/src/components/shared/TabBar";
+
+// const CustomTabBar = (props: any) => <TabBar {...props} />;
+
+// const TabLayout = () => {
+//   return (
+//     <Tabs
+//       tabBar={CustomTabBar}
+//       screenOptions={{
+//         headerShown: false,
+//       }}
+//       initialRouteName="home"
+//       backBehavior="history"
+//     >
+//       <Tabs.Screen name="home" />
+//     </Tabs>
+//   );
+// };
+
+// export default TabLayout;
+=======
+import React from "react";
+import { Tabs } from "expo-router";
+import TabBar from "@/src/components/shared/TabBar";
+
+const CustomTabBar = (props: any) => <TabBar {...props} />;
+
+const TabLayout = () => {
+  return (
+    <Tabs
+      tabBar={CustomTabBar}
+      screenOptions={{
+        headerShown: false,
+      }}
+      initialRouteName="home"
+      backBehavior="history"
+    >
+      <Tabs.Screen name="home" />
+    </Tabs>
+  );
+};
+
+export default TabLayout;
+>>>>>>> 7882329 (funcional 1)
